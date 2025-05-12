989845cfb79dc73262652336563b5bdb07c91f07
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2025-05-12 10:25:14 +0200

    feat: console setup
    Simple console with actions that also allows to pick another or quit the app.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var endApp = false;
 
-Console.WriteLine("Hello, World!");
+        Console.WriteLine("Database Benchmarking.\r");
+        Console.WriteLine("----------------------\n");
+
+        while (!endApp)
+        {
+            Console.WriteLine("Pick a database to benchmark:");
+            Console.WriteLine("\t1. Relational");
+            Console.WriteLine("\t2. NoSQL");
+            Console.WriteLine();
+            Console.Write("Your choice: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Relational");
+                    break;
+                case "2":
+                    Console.WriteLine("NoSQL");
+                    break;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("-------------------\n");
+            Console.WriteLine("Press 'q' to quit the app, or any key to continue...");
+            
+            if (Console.ReadLine() == "q") endApp = true;
+            
+            Console.WriteLine("\n");
+        }
+
+        return;
+    }
+}
