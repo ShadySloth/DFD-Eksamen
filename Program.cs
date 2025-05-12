04f592325680e4dacf9bbb035f9b140dd702a445
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2025-05-12 10:30:25 +0200

    feat: added methods for benchmarking

--- Program.cs
+++ Program.cs
@@ -18,10 +18,10 @@
             switch (Console.ReadLine())
             {
                 case "1":
-                    Console.WriteLine("Relational");
+                    BenchmarkRelational();
                     break;
                 case "2":
-                    Console.WriteLine("NoSQL");
+                    BenchmarkNoSql();
                     break;
             }
 
@@ -35,5 +35,73 @@
         }
 
         return;
+    }
+
+    private static void BenchmarkRelational()
+    {
+        // Code to benchmark relational databases
+        Console.WriteLine();
+        Console.WriteLine("Pick a benchmarking method:");
+        Console.WriteLine("\t1. Insert");
+        Console.WriteLine("\t2. Query");
+        Console.WriteLine("\t3. Update");
+        Console.WriteLine("\t4. Delete");
+        Console.WriteLine("\t5. All");
+        Console.WriteLine();
+        
+        Console.Write("Your choice: ");
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Console.WriteLine("Benchmarking Insert...");
+                break;
+            case "2":
+                Console.WriteLine("Benchmarking Query...");
+                break;
+            case "3":
+                Console.WriteLine("Benchmarking Update...");
+                break;
+            case "4":
+                Console.WriteLine("Benchmarking Delete...");
+                break;
+            case "5":
+                Console.WriteLine("Benchmarking All...");
+                break;
+        }
+        // Add your benchmarking logic here
+    }
+
+    private static void BenchmarkNoSql()
+    {
+        // Code to benchmark NoSQL databases
+        Console.WriteLine();
+        Console.WriteLine("Pick a benchmarking method:");
+        Console.WriteLine("\t1. Insert");
+        Console.WriteLine("\t2. Query");
+        Console.WriteLine("\t3. Update");
+        Console.WriteLine("\t4. Delete");
+        Console.WriteLine("\t5. All");
+        Console.WriteLine();
+        
+        Console.Write("Your choice: ");
+        switch (Console.ReadLine())
+        {
+            case "1":
+                Console.WriteLine("Benchmarking Insert...");
+                break;
+            case "2":
+                Console.WriteLine("Benchmarking Query...");
+                break;
+            case "3":
+                Console.WriteLine("Benchmarking Update...");
+                break;
+            case "4":
+                Console.WriteLine("Benchmarking Delete...");
+                break;
+            case "5":
+                Console.WriteLine("Benchmarking All...");
+                break;
+        }
+        // Add your benchmarking logic here
     }
 }
