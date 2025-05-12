d4f00402bb2a26c68d9c374abc6940a3f60c00bd
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2025-05-12 10:28:25 +0200

    Models_with_entity_id

---
+++ Domain/Entities/EntityId.cs
@@ -0,0 +1,11 @@
+﻿namespace Database_Benchmarking.Domain.Entities;
+
+public class EntityId
+{
+    public string Value { get; private set; }
+    
+    public EntityId(string value)
+    {
+        Value = value;
+    }
+}
