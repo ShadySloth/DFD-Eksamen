5e19d865237ec181be2050107b958dd621fa746f
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2025-05-13 12:17:08 +0200

    remove get_by_id

--- Infrastructure/Repository/PostgresSQL/PostgresArticleRepository.cs
+++ Infrastructure/Repository/PostgresSQL/PostgresArticleRepository.cs
@@ -23,19 +23,6 @@ namespace Database_Benchmarking.Infrastructure.Repository.PostgresSQL;
             return stopwatch.Elapsed; // Returnerer den tid, det tog at hente alle artikler
         }
 
-        public TimeSpan GetById(ICollection<EntityId> ids)
-        {
-            var stopwatch = new System.Diagnostics.Stopwatch();
-            stopwatch.Start();
-
-            var articles = _context.Articles
-                .Where(a => ids.Contains(a.Id))
-                .ToList();
-
-            stopwatch.Stop();
-            return stopwatch.Elapsed; // Returnerer den tid, det tog at hente artiklerne med de angivne id'er
-        }
-
         public TimeSpan Create(ICollection<Article> articles)
         {
             var stopwatch = new System.Diagnostics.Stopwatch();
