682a5c149e9d80961ae54220c6c9b2325c974db9
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-05-13 09:34:11 +0200

    fix: Minor changes in Compose and removed somethin

--- Domain/Service/Services/ArticleService.cs
+++ Domain/Service/Services/ArticleService.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using Database_Benchmarking.Domain.Entities;
 using Database_Benchmarking.Infrastructure.Repository.Interfaces;
 using Database_Benchmarking.Domain.Service.Interfaces;
@@ -15,31 +13,26 @@ namespace Database_Benchmarking.Domain.Service.Services
             _repository = repository;
         }
 
-        // Hent alle artikler
         public TimeSpan GetAllArticles()
         {
             return _repository.GetAll();
         }
 
-        // Hent artikler baseret på ID'er
         public TimeSpan GetArticleById(ICollection<EntityId> ids)
         {
             return _repository.GetById(ids);
         }
 
-        // Opret artikler
         public TimeSpan CreateArticle(ICollection<Article> articles)
         {
             return _repository.Create(articles);
         }
 
-        // Opdater artikler
         public TimeSpan UpdateArticle(ICollection<Article> articles)
         {
             return _repository.Update(articles);
         }
 
-        // Slet artikler
         public TimeSpan DeleteArticle(ICollection<EntityId> ids)
         {
             return _repository.Delete(ids);
--- db/init-db.sh
+++
@@ -1,3 +0,0 @@
-﻿#!/bin/bash
-echo "host    all             all             172.31.0.1          md5" >> /var/lib/postgresql/data/pg_hba.conf
-pg_ctl reload
