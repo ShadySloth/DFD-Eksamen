466faf06dc6071bb3d465967e1bb186d20d77ead
Peder Antonisen <113884778+ShadySloth@users.noreply.github.com>
2025-05-13 12:26:17 +0200

    refactor: benchmark method
    abstracted the benchmark method to reduce duplicate code.

--- Consoles/BenchmarkConsole.cs
+++ Consoles/BenchmarkConsole.cs
@@ -30,10 +30,10 @@ public static class BenchmarkConsole
             switch (choice)
             {
                 case "1":
-                    BenchmarkRelational();
+                    Benchmark(DatabaseType.Relational);
                     break;
                 case "2":
-                    BenchmarkNoSql();
+                    Benchmark(DatabaseType.NoSql);
                     break;
             }
 
@@ -93,9 +93,9 @@ public static class BenchmarkConsole
         }
     }
 
-    private static void BenchmarkRelational()
+    private static void Benchmark(DatabaseType databaseType)
     {
-        var service = new ServiceController(DatabaseType.Relational);
+        var service = new ServiceController(databaseType);
         Console.WriteLine();
         Console.WriteLine("Pick a benchmarking method:");
         Console.WriteLine("\t1. Insert");
@@ -113,80 +113,37 @@ public static class BenchmarkConsole
         switch (choice)
         {
             case "1":
-                Console.WriteLine("Benchmarking Insert...");
                 count = GetNumberInput();
+                Console.WriteLine($"Benchmarking {count} Inserts...");
                 time = service.CreateArticles(count);
+                Console.WriteLine($"Time taken: {time.TotalMilliseconds}ms");
                 break;
             case "2":
-                Console.WriteLine("Benchmarking Query...");
-                time = service.GetAllArticles();
+                count = GetNumberInput();
+                Console.WriteLine($"Benchmarking {count} Fetches...");
+                time = service.GetAllArticles(count);
+                Console.WriteLine($"Time taken: {time.TotalMilliseconds}ms");
                 break;
             case "3":
-                Console.WriteLine("Benchmarking Update...");
                 count = GetNumberInput();
+                Console.WriteLine($"Benchmarking {count} Updates...");
                 time = service.UpdateArticles(count);
+                Console.WriteLine($"Time taken: {time.TotalMilliseconds}ms");
                 break;
             case "4":
-                Console.WriteLine("Benchmarking Delete...");
                 count = GetNumberInput();
+                Console.WriteLine($"Benchmarking {count} Deletes...");
                 time = service.DeleteArticles(count);
+                Console.WriteLine($"Time taken: {time.TotalMilliseconds}ms");
                 break;
             case "5":
-                Console.WriteLine("Benchmarking All...");
                 count = GetNumberInput();
+                Console.WriteLine("Benchmarking All...");
                 time = service.CreateArticles(count);
-                time += service.GetAllArticles();
+                time += service.GetAllArticles(count);
                 time += service.UpdateArticles(count);
                 time += service.DeleteArticles(count);
-                break;
-        }
-    }
-
-    private static void BenchmarkNoSql()
-    {
-        var service = new ServiceController(DatabaseType.NoSql);
-        Console.WriteLine();
-        Console.WriteLine("Pick a benchmarking method:");
-        Console.WriteLine("\t1. Insert");
-        Console.WriteLine("\t2. Query");
-        Console.WriteLine("\t3. Update");
-        Console.WriteLine("\t4. Delete");
-        Console.WriteLine("\t5. All");
-        Console.WriteLine();
-
-        Console.Write("Your choice: ");
-        var choice = GetValidInput(["1", "2", "3", "4", "5"]);
-
-        TimeSpan time;
-        var count = 0;
-        switch (choice)
-        {
-            case "1":
-                Console.WriteLine("Benchmarking Insert...");
-                count = GetNumberInput();
-                time = service.CreateArticles(count);
-                break;
-            case "2":
-                Console.WriteLine("Benchmarking Query...");
-                time = service.GetAllArticles();
-                break;
-            case "3":
-                Console.WriteLine("Benchmarking Update...");
-                count = GetNumberInput();
-                time = service.UpdateArticles(count);
-                break;
-            case "4":
-                Console.WriteLine("Benchmarking Delete...");
-                count = GetNumberInput();
-                time = service.DeleteArticles(count);
-                break;
-            case "5":
-                Console.WriteLine("Benchmarking All...");
-                count = GetNumberInput();
-                time = service.CreateArticles(count);
-                time += service.GetAllArticles();
-                time += service.UpdateArticles(count);
-                time += service.DeleteArticles(count);
+                Console.WriteLine($"Total time taken: {time.TotalMilliseconds}ms");
                 break;
         }
     }
