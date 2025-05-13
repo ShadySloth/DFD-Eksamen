93b921466b27cbd602cdcefdfec014f8ec28eb91
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2025-05-13 10:18:43 +0200

    mock

---
+++ Domain/Service/Services/MockDataService.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Database_Benchmarking.Domain.Entities;
+using Database_Benchmarking.Infrastructure.MockObjectBuilders.Interfaces;
+using Database_Benchmarking.Infrastructure.MockObjectBuilders.Builders; // <- Husk at denne namespace skal matche dine konkrete klasser
+
+namespace Database_Benchmarking.Domain.Service.Services
+{
+    public class MockDataService
+    {
+        private readonly IAuthorMockBuilder _authorMockBuilder;
+        private readonly IGenreMockBuilder _genreMockBuilder;
+        private readonly IArticleMockBuilder _articleMockBuilder;
+
+        public MockDataService()
+        {
+            _authorMockBuilder = new MockAuthorBuilder();
+            _genreMockBuilder = new MockGenreBuilder();
+            _articleMockBuilder = new MockArticleBuilder();
+        }
+
+        public List<Author> GenerateMockAuthors(int authorCount)
+        {
+            string authorPrefix = "Author"; // Mock prefix for author names
+            return _authorMockBuilder.BuildAuthors(authorCount, authorPrefix);
+        }
+
+        public List<Genre> GenerateMockGenres(int genreCount)
+        {
+            return _genreMockBuilder.BuildGenres(genreCount); // No extra parameters for genres
+        }
+
+        public List<Article> GenerateMockArticles(int articleCount)
+        {
+            int authorCount = 5; // Fixed number of authors for mock data
+            int genreCount = 3; // Fixed number of genres for mock data
+            string baseTitle = "Article"; // Mock base title for articles
+
+            var genres = GenerateMockGenres(genreCount);
+            var authors = GenerateMockAuthors(authorCount);
+
+            return _articleMockBuilder.BuildArticles(articleCount, baseTitle, genres, authors);
+        }
+    }
+}
---
+++ Infrastructure/MockObjectBuilders/Builders/MockGenreBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Database_Benchmarking.Domain.Entities;
+using Database_Benchmarking.Infrastructure.MockObjectBuilders.Interfaces;
+
+namespace Database_Benchmarking.Infrastructure.MockObjectBuilders.Builders
+{
+    public class MockGenreBuilder: IGenreMockBuilder
+    {
+        public List<Genre> BuildGenres(int count)
+        {
+            var genreTypes = new List<string>
+            {
+                "Action", "Comedy", "Drama", "Horror", "Fantasy", "Sci-Fi", "Adventure", "Romance", "Thriller", "Mystery"
+            };
+
+            var genres = new List<Genre>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var genre = new Genre
+                {
+                    Type = $"{genreTypes[i % genreTypes.Count]} {i + 1}"
+                };
+
+                genres.Add(genre);
+            }
+
+            return genres;
+        }
+
+    }
+}
