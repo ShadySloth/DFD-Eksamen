103491005d9ccb3bc7c3fa324743148b822d78f2
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-05-14 10:02:59 +0200

    feat: ServiceController GetByID

--- Domain/Service/Interfaces/IServiceController.cs
+++ Domain/Service/Interfaces/IServiceController.cs
@@ -4,12 +4,14 @@ public interface IServiceController
 {
     TimeSpan CreateArticles(int count);
     TimeSpan GetAllArticles(int count);
+    TimeSpan GetByIdArticles(int count, int indexToGet);
     TimeSpan DeleteArticles(int count);
     TimeSpan UpdateArticles(int count);
     
     
     TimeSpan CreateAuthors(int count);
     TimeSpan GetAllAuthors(int count);
+    TimeSpan GetByIdAuthors(int count, int indexToGet);
     TimeSpan DeleteAuthors(int count);
     TimeSpan UpdateAuthors(int count);
 }
