0932ce3bc9c3149580c2f393e7787beff6e01408
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2025-05-14 09:51:40 +0200

    feat: GetById service layer

--- Domain/Service/Interfaces/IArticleService.cs
+++ Domain/Service/Interfaces/IArticleService.cs
@@ -5,6 +5,7 @@ namespace Database_Benchmarking.Domain.Service.Interfaces;
 public interface IArticleService
 {
     TimeSpan GetAllArticles(ICollection<Article> articles);
+    TimeSpan GetById(ICollection<Article> articles, int indexToGet);
     TimeSpan CreateArticle(ICollection<Article> articles);
     TimeSpan UpdateArticle(ICollection<Article> articles);
     TimeSpan DeleteArticle(ICollection<Article> articles);
